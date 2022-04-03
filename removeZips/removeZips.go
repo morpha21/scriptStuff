c47810d9565ee9c3924bdc005dc4857cf350cb72
morpha <v21.leonardo@gmail.com>
2022-04-03 13:47:54 -0300

    removeZips.go now reads directories from CLI args

--- removeZips/removeZips.go
+++ removeZips/removeZips.go
@@ -1,42 +1,41 @@
 package main
 
 import (
- "io/ioutil"
- "os"
- "path/filepath"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 )
-//
-//          checks if nome is in lista. 
-//
+
+//          checks if nome is in lista.
+
 func checkin(nome string, lista []string) bool {
- for _, i := range lista {
-  if i == nome {
-   return true
-  }
- }
- return false
+	for _, i := range lista {
+		if i == nome {
+			return true
+		}
+	}
+	return false
 }
-//
-//
-//
+
 func main() {
- path := "/home/morpha/testen/"
- files, _ := ioutil.ReadDir(path)
+	for _, path := range os.Args[1:] {
 
- zips := make([]string, 0)
- folders := make([]string, 0)
+		files, _ := ioutil.ReadDir(path)
+		zips := make([]string, 0)
+		folders := make([]string, 0)
 
- for _, f := range files {
-  if filepath.Ext(f.Name()) == ".zip" {
-   zips = append(zips, f.Name())
-  } else {
-   folders = append(folders, f.Name())
-  }
- }
+		for _, f := range files {
+			if filepath.Ext(f.Name()) == ".zip" {
+				zips = append(zips, f.Name())
+			} else {
+				folders = append(folders, f.Name())
+			}
+		}
 
- for _, folder := range folders {
-  if checkin(folder+".zip", zips) {
-   os.Remove(path + folder + ".zip")
-  }
- }
+		for _, folder := range folders {
+			if checkin(folder+".zip", zips) {
+				os.Remove(path + folder + ".zip")
+			}
+		}
+	}
 }
